886993a1f858a319d74acd46973ad0b4cf627d1c
Aravindhan Venkituswamy <avenkituswamy@netspend.com>
2019-05-20 15:04:30 -0500

    initial commit

---
+++ src/main/java/kafka/KafkaProducerDemo.java
@@ -0,0 +1,15 @@
+package kafka;
+
+public class KafkaProducerDemo {
+    public static final String TOPIC = "sampleTopic";
+    
+    public static void main(String[] args) {
+        boolean isAsync = false;
+        SampleProducer producerThread = new SampleProducer(TOPIC, isAsync);
+        // start the producer
+        producerThread.start();
+
+        //producerThread.stop();
+ 
+    }
+}
